96996010df5dc8ea006916c62b94bb4556ca5da3
Andrei Branza <andrei.branza@yahoo.com>
2024-11-02 20:37:51 +0200

    Added findByEmail method to UserRepo

--- src/main/java/com/braziigemeni/bookingapp/repository/UserRepository.java
+++ src/main/java/com/braziigemeni/bookingapp/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.braziigemeni.bookingapp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.braziigemeni.bookingapp.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 
 }
