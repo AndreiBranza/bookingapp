122b3bc6c822ffdb78a6685a2f2a6d263ba7e848
Andrei Branza <andrei.branza@yahoo.com>
2024-11-03 19:34:57 +0200

    User service updates

--- src/main/java/com/braziigemeni/bookingapp/service/UserService.java
+++ src/main/java/com/braziigemeni/bookingapp/service/UserService.java
@@ -1,13 +1,21 @@
 package com.braziigemeni.bookingapp.service;
 
-import java.util.Optional;
+import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.braziigemeni.bookingapp.dto.ChangePasswordDTO;
+import com.braziigemeni.bookingapp.dto.CreateUserDTO;
+import com.braziigemeni.bookingapp.dto.UserDTO;
+import com.braziigemeni.bookingapp.exception.InvalidPasswordException;
+import com.braziigemeni.bookingapp.exception.UserNotFoundException;
 import com.braziigemeni.bookingapp.model.User;
+import com.braziigemeni.bookingapp.model.UserRole;
 import com.braziigemeni.bookingapp.repository.UserRepository;
+import com.braziigemeni.bookingapp.util.JwtTokenProvider;
+import com.braziigemeni.bookingapp.util.UserMapper;
 
 @Service
 @RequiredArgsConstructor
@@ -15,20 +23,48 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider tokenProvider;
+    private final UserMapper userMapper;  // vom folosi mapstruct
 
-    public User createUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+    public UserDTO createUser(CreateUserDTO createUserDTO) {
+        User user = userMapper.toEntity(createUserDTO);
+        user.setPassword(passwordEncoder.encode(createUserDTO.password()));
+        return userMapper.toDto(userRepository.save(user));
     }
 
-    public boolean authenticateUser(String email, String rawPassword) {
-        return userRepository.findByEmail(email)
-            .map(user -> passwordEncoder.matches(rawPassword, user.getPassword()))
-            .orElse(false);
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll().stream()
+            .map(userMapper::toDto)
+            .toList();
     }
 
-    // Metodă utilă pentru găsirea user-ului după email
-    public Optional<User> findByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public UserDTO getUserById(Long id) {
+        return userRepository.findById(id)
+            .map(userMapper::toDto)
+            .orElseThrow(() -> new UserNotFoundException("User not found"));
     }
+
+    public UserDTO updateUserRole(Long userId, UserRole newRole) {
+        return userRepository.findById(userId)
+            .map(user -> {
+                user.setRole(newRole);
+                return userMapper.toDto(userRepository.save(user));
+            })
+            .orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+
+    // această metodă nu returnează DTO pentru că e doar acțiune
+    public void changePassword(Long userId, ChangePasswordDTO changePasswordDTO) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        if (!passwordEncoder.matches(changePasswordDTO.oldPassword(),
+            user.getPassword())) {
+            throw new InvalidPasswordException("Invalid old password");
+        }
+
+        user.setPassword(passwordEncoder.encode(changePasswordDTO.newPassword()));
+        userRepository.save(user);
+    }
+
 }
