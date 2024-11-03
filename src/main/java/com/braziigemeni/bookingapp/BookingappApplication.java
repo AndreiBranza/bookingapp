a9b6f198fa2aa7be713f8a7d80a9d99482ab17af
Andrei Branza <andrei.branza@yahoo.com>
2024-11-03 19:34:27 +0200

    Eliminated test from main app

--- src/main/java/com/braziigemeni/bookingapp/BookingappApplication.java
+++ src/main/java/com/braziigemeni/bookingapp/BookingappApplication.java
@@ -18,34 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 public class BookingappApplication {
 
-	@Autowired private UserRepository userRepository;
-	@Autowired private PasswordEncoder passwordEncoder;
-	@Autowired private JwtTokenProvider tokenProvider;
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookingappApplication.class, args);
-	}
-
-	@PostConstruct
-	public void testDatabaseConnection() {
-		try {
-			log.info("Testing database connection...");
-			UserService userService = new UserService(userRepository, passwordEncoder, tokenProvider);
-
-			User testUser = new User();
-			testUser.setEmail("test@test.com");
-			testUser.setFirstName("Test");
-			testUser.setLastName("Test");
-			testUser.setPassword("test");
-
-			User saved = userService.createUser(testUser);
-			userRepository.delete(saved);
-
-			log.info("Database connection test: SUCCESS ✅");
-		} catch (Exception e) {
-			log.error("Database connection test: FAILED ❌", e);
-			// Decizi dacă vrei să oprești aplicația în caz de eșec
-			// System.exit(1);
-		}
 	}
 }
